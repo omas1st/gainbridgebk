8e646097d5fab049e62284ea0feef52578626fa1
omas1st <omas1st@gmail.com>
2025-09-29 04:04:23 +0100

    update public controller

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,4 +1,3 @@
-// controllers/publicController.js
 const User = require('../models/User')
 
 exports.stats = async (req, res, next) => {
@@ -6,9 +5,9 @@ exports.stats = async (req, res, next) => {
     // total registered users (include both user & agent)
     const totalUsers = await User.countDocuments({})
 
-    // members online = users with lastLogin within the last 1 hour
-    const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000)
-    const onlineUsers = await User.countDocuments({ lastLogin: { $gte: oneHourAgo } })
+    // members online = users with lastLogin within the last 10 hours (was 1 hour)
+    const tenHoursAgo = new Date(Date.now() - 10 * 60 * 60 * 1000)
+    const onlineUsers = await User.countDocuments({ lastLogin: { $gte: tenHoursAgo } })
 
     res.json({ totalUsers, onlineUsers })
   } catch (err) {
